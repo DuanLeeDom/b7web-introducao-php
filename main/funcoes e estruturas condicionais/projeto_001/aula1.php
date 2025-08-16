0d5d7b26917550d2976a7f1b2d22da75244bafb6
duanleedom <duanleedom@gmail.com>
2025-08-16 18:56:22 -0300

    Módulo 2

---
+++ main/funcoes e estruturas condicionais/projeto_001/aula1.php
@@ -0,0 +1,17 @@
+<?php
+$nome = 'Jorge';
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> Olá, Mundo</h1>
+    <h2> Eu sou <?php echo $nome;?></h2>
+</body>
+
+</html>
